d7b604a0db0253cb0770fdb5542d39d784e3f534
denis burnashev <kardenman@gmail.com>
2020-12-04 10:07:05 +0600

    v 2.0

--- main.py
+++ main.py
@@ -1,37 +1,64 @@
-def counting_top_words_in_xml(file_name):
-    from collections import Counter
+from collections import Counter
+import json
+
+sorted_list = []
+
+
+def counting_top_words_in_xml(file_name, user_input):
     import xml.etree.ElementTree as ET
     parser = ET.XMLParser(encoding='utf-8')
     tree = ET.parse(file_name, parser)
     root = tree.getroot()
     news_xml = root.findall('channel/item')
-
     for news in news_xml:
         news_inf = news.find('description').text
         news_list = news_inf.lower().split(' ')
-        news_list.sort()
-
-    sorted_list = []
-
     for words in news_list:
-        if len(words) >= 6:
+        if len(words) >= user_input:
             sorted_list.append(words)
-            sorted_words = Counter(sorted_list).most_common(10)
+    user_input = input('Если вы хотите посмотреть отфильтрованный список введите list, если хотите увидеть топ слов введите top:\n')
+    if user_input == 'list':
+        print(sorted_list)
+        sorted_list.clear()
+    elif user_input == 'top':
+        top_words()
+
+
+def counting_top_words_in_json(file_name, user_input):
+    with open(file_name, encoding='utf-8') as sorting_words:
+        json_data = json.load(sorting_words)
+        all_news = json_data['rss']['channel']['items']
+        for description in all_news:
+            words = description['description'].split(' ')
+        for word in words:
+            if len(word) >= user_input:
+                sorted_list.append(word)
+        user_input = input('Если вы хотите посмотреть отфильтрованный список введите list, если хотите увидеть топ слов введите top:\n')
+        if user_input == 'list':
+            print(sorted_list)
+            sorted_list.clear()
+        elif user_input == 'top':
+            top_words()
+
+
+def top_words():
+    user_input = input('Введите число топ скольки слов вы хотите увидеть:\n')
+    sorted_words = Counter(sorted_list).most_common(int(user_input))
     print(sorted_words)
+    sorted_list.clear()
 
-    sorted_words = {}
 
-    for words in sorted_list:
-        if words in sorted_words:
-            sorted_words[words] += 1
-        else:
-            sorted_words[words] = 1
-    inverse = [(value, key) for key, value in sorted_words.items()]
-    reverse_inverse = sorted(inverse, reverse=True)
-    sorted_list_1 = []
-    for word in reverse_inverse[0:10]:
-        qwe = (word[1], word[0])
-        sorted_list_1.append(qwe)
-    print(sorted_list_1)
+def main():
+    while True:
+        user_input = input('Введите вашу команду: \n x для получения данных из xml файла \n j для получения данных из json файла\n exit для выхода из программы\n')
+        if user_input == 'x':
+            user_input = int(input('Введите число букв в слове для фильтрации списка слов:'))
+            counting_top_words_in_xml('newsafr.xml', user_input)
+        elif user_input == 'j':
+            user_input = int(input('Введите число букв в слове для фильтрации списка слов:'))
+            counting_top_words_in_json('newsafr.json', user_input)
 
-counting_top_words_in_xml('newsafr.xml')
+        elif user_input == 'exit':
+            break
+
+main()
